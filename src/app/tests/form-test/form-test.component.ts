9b457c4892a029e44e1bced7797fffdba6ee165b
staran0501 <sonyuser8@yahoo.com.tw>
2019-11-19 01:28:52 +0800

    loop through FormArray using controls

--- src/app/tests/form-test/form-test.component.ts
+++ src/app/tests/form-test/form-test.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-form-test',
@@ -7,19 +7,43 @@ import { FormGroup, FormControl} from '@angular/forms';
   styleUrls: ['./form-test.component.css']
 })
 export class FormTestComponent implements OnInit {
+  constructor() { }
 
   form = new FormGroup({
     site: new FormControl(),
     zone: new FormControl(),
-    location: new FormControl()
+    hosts: new FormArray([])
+    // location: new FormControl(),
   });
-  constructor() { }
+  index: number = 0;
 
   ngOnInit() {
   }
-
-  onClick(){
+  onClick() {
+    this.index++;
+    (this.form.get('hosts') as FormArray).push(
+      new FormGroup({
+        location: new FormControl(this.index.toString()),
+        name: new FormControl(this.index.toString())
+      })
+    );
+    // new Form
     console.log(this.form.get('site').value);
-  }
+    console.log(this.form.get('zone').value);
 
+    // (this.form.get('hosts') as FormArray)
+
+    // (this.form.get('hosts') as FormArray).controls
+
+    // tslint:disable-next-line: forin
+    for (const control in (this.form.get('hosts') as FormArray).controls) {
+      const host = (this.form.get('hosts') as FormArray).get(control);
+      console.log(host);
+      console.log(host.get('location').value);
+      console.log(host.get('name').value);
+    }
+  }
 }
+
+
+// FormControl, FormGroup, FormArray
