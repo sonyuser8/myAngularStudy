8f8a87d944785bba5a7515cb6b0c5abf97db8edd
staran0501 <sonyuser8@yahoo.com.tw>
2019-11-20 22:21:51 +0800

    pure function practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import * as Const from './const';
+import { AppConst, Fuck, CFG } from './const';
 declare let $: any;
 
 @Component({
@@ -9,4 +11,19 @@ declare let $: any;
 export class AppComponent {
   title: string = 'test2019111601';
 
+  test() {
+    let state = new AppConst(1);
+    // console.log(CFG.cfg1);
+    console.log('before:' + state.count);
+    this.increment(state);
+    console.log('after:' + state.count);
+  }
+
+  increment(state: AppConst): AppConst {
+    let NewObject = Object.assign({}, state, { count: state.count + 1 });
+    // let NewObject = Object.assign({}, state, { });
+    console.log(NewObject);
+    return NewObject;
+    // return state.count + 1;
+  }
 }
---
+++ src/app/const.ts
@@ -0,0 +1,19 @@
+
+export class AppConst {
+  count: number = 0;
+  myStr: string = 'test';
+
+  constructor(cnt: number) {
+    this.count = cnt;
+  }
+}
+
+export function doHAHA(){
+  console.log('TEST');
+}
+
+export const Fuck = 'FUCK';
+
+export const CFG = { cfg1: 'this is cfg1', cfg2: 'this is cfg2' };
+
+
