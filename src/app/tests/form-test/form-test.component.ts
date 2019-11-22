beff1c318150f724ea54858fea9236f1db0f7864
staran0501 <sonyuser8@yahoo.com.tw>
2019-11-22 23:42:35 +0800

    get errors from dynamic add form control

--- src/app/tests/form-test/form-test.component.ts
+++ src/app/tests/form-test/form-test.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-test',
@@ -8,54 +8,50 @@ import { FormGroup, FormControl, FormArray } from '@angular/forms';
 })
 export class FormTestComponent implements OnInit {
   constructor() { }
+  get site() {
+    return this.form.get('site');
+  }
 
+  index: number = 0;
   form = new FormGroup({
-    site: new FormControl(),
+    site: new FormControl('', [Validators.required, Validators.minLength(3)]),
     compartment: new FormControl(),
     zone: new FormControl(),
     hosts: new FormArray([])
     // location: new FormControl(),
   });
-  index: number = 0;
-
-  ngOnInit() {
-  }
   onClick() {
     this.index++;
     (this.form.get('hosts') as FormArray).push(
       new FormGroup({
-        location: new FormControl(''),
-        name: new FormControl('')
+        location: new FormControl('', Validators.required),
+        name: new FormControl('', Validators.required)
       })
     );
-    // New Form
     console.log(this.form.get('site').value);
     console.log(this.form.get('zone').value);
-
-    // (this.form.get('hosts') as FormArray)
-
-    // (this.form.get('hosts') as FormArray).controls
-
-    // tslint:disable-next-line: forin
-    // for (const control in (this.form.get('hosts') as FormArray).controls) {
-    //   const host = (this.form.get('hosts') as FormArray).get(control);
-    //   console.log(host);
-    //   console.log(host.get('location').value);
-    //   console.log(host.get('name').value);
-    // }
 
     for (let item of (this.form.get('hosts') as FormArray).controls) {
       // console.log(item);
       console.log(item.get('location').value);
       console.log(item.get('name').value);
     }
-
+  }
+  onTest() {
+    // console.log(( (this.form.get('hosts') as FormArray).controls[0] as FormGroup).get('location') );
+    // (this.form.get('hosts') as FormArray)
+  }
+  onTest2(obj) {
+    console.log('CKPT1:', obj);
+    return false;
   }
 
+  ngOnInit() {
+  }
   onSubmit() {
-    let result = this.form.value;
-    console.log(result);
-    console.log(JSON.stringify(result));
+    // let result = this.form.value;
+    console.log('Form Valid?', this.form.valid);
+    console.log(JSON.stringify(this.form.value));
   }
 }
 
